4e7f7fc72949360fcac9535d9ce1fbef98097f7d
ghoshbinayak <ghoshbinayak@gmail.com>
2015-01-20 23:31:44 +0530

    bugfix

--- cvsetup.cpp
+++ cvsetup.cpp
@@ -13,7 +13,7 @@ void onSizeChange(int, void*);
 
 int main(int argc, char** argv) {
 
-  if (argc < 1) {
+  if (argc < 2) {
     cout << "Usage: cvsetup <path to image>" << endl;
     return 0;
   }
