0dae29014fb0de655e8a6fc62eafe3d6ecf3a3d6
ghoshbinayak <ghoshbinayak@gmail.com>
2015-01-21 13:34:50 +0530

    implemented the full algo but its not working, starting the debuging process

--- cvsetup.cpp
+++ cvsetup.cpp
@@ -6,15 +6,17 @@
 using namespace cv;
 using namespace std;
 
-int size = 10, threshval= 200;
-Mat image, result, final;
+int size = 10, threshval = 200;
+Mat image, result, final, steps;
 
-Point2i getTopLeftPoint(Mat& image);
 void DrawGrid(Mat& image, int size);
 void onSizeChange(int, void*);
-Point2i getStartPoint(Mat& img, Point2i p, int gsize);
 int getPointType(Mat& img, Point2i q, int gsize);
 bool ObjectInUGB(Mat& img, Point2i q, int ugb, int gsize);
+Point2i getTopLeftPoint(Mat& image);
+Point2i getStartPoint(Mat& img, Point2i p, int gsize);
+vector<Point2i> makeOIP(Mat& img, Point2i topleftpoint, int gsize);
+Point2i getNextPoint(Point2i currentpoint, int d, int gsize);
 
 int main(int argc, char** argv) {
 
@@ -47,16 +49,20 @@ int main(int argc, char** argv) {
   imshow("Final image", final);
 
   // Create a trakbar to control the grid size
-  createTrackbar( "Grid size", "Final image", &size, 50, onSizeChange);
-  createTrackbar( "Threshold", "Final image", &threshval, 255, onSizeChange);
-  waitKey(0);
+  createTrackbar("Grid size", "Final image", &size, 50, onSizeChange);
+  createTrackbar("Threshold", "Final image", &threshval, 255, onSizeChange);
 
-  Point2i p = getTopLeftPoint(result);
-  cout << p << endl;
-  p = getStartPoint(result, p, size);
-  cout << p << endl;
+  waitKey(1000);
+  // cout << p << endl;
+  // p = getStartPoint(result, p, size);
+  // cout << p << endl;
   // Point2i arbit(150, 50);
-  cout << getPointType(result, p, size) << endl;
+  // cout << getPointType(result, p, size) << endl;
+  Point2i p = getTopLeftPoint(result);
+  vector<Point2i> isotheticcover = makeOIP(result, p, size);
+
+  cout << isotheticcover << endl;
+  waitKey(1000);
   return 0;
 }
 
@@ -100,27 +106,27 @@ void DrawGrid(Mat& img, int gsize) {
     return;
   int nRows = img.rows;
   int nCols = img.cols;
-  
-  Point2i p1(0,0);
-  Point2i p2(0,nRows);
+
+  Point2i p1(0, 0);
+  Point2i p2(0, nRows);
   Point2i stepx(gsize, 0);
-  for (int i = gsize; i < nCols; i += gsize)
-  {
-    p1 += stepx; p2+=stepx;
+  for (int i = gsize; i < nCols; i += gsize) {
+    p1 += stepx;
+    p2 += stepx;
     line(img, p1, p2, CV_RGB(255, 0, 255), 1, CV_AA, 0);
   }
 
   p1 = Point2i(0, 0);
-  p2 = Point2i(nCols,0);
+  p2 = Point2i(nCols, 0);
   Point2i stepy(0, gsize);
-  for (int i = gsize; i < nRows; i += gsize)
-  {
-    p1 += stepy; p2+= stepy;
+  for (int i = gsize; i < nRows; i += gsize) {
+    p1 += stepy;
+    p2 += stepy;
     line(img, p1, p2, CV_RGB(255, 0, 255), 1, CV_AA, 0);
   }
 }
 
-void onSizeChange(int, void *){
+void onSizeChange(int, void*) {
 
   final = image.clone();
   cvtColor(image, result, CV_RGB2GRAY);
@@ -131,46 +137,44 @@ void onSizeChange(int, void *){
   imshow("Final image", final);
 }
 
-Point2i getStartPoint(Mat& img, Point2i p, int gsize){
+Point2i getStartPoint(Mat& img, Point2i p, int gsize) {
   int qx, qy;
-  qx = (ceil(float(p.x)/gsize) -1)*gsize;
-  qy = (floor(float(p.y)/gsize))*gsize;
+  qx = (ceil(float(p.x) / gsize) - 1) * gsize;
+  qy = (floor(float(p.y) / gsize)) * gsize;
   Point2i q(qx, qy);
   return q;
 }
 
-int getPointType(Mat& img, Point2i q, int gsize){
-  int m=0, r=0, t=10;
-  for(int k=1; k<5; k++){
-    if( ObjectInUGB(img, q, k, gsize)){
-      m++; 
+int getPointType(Mat& img, Point2i q, int gsize) {
+  int m = 0, r = 0, t = 10;
+  for (int k = 1; k < 5; k++) {
+    if (ObjectInUGB(img, q, k, gsize)) {
+      m++;
       r += k;
       cout << "debug info: inside" << endl;
     }
   }
   if (m = 2 && (r == 4 || r == 6)) {
-    t = -2; 
-  }
-  else if (m == 0 || m == 4){
+    t = -2;
+  } else if (m == 0 || m == 4) {
     t = 0;
     // cout << "debug info: " << m << endl;
-  }
-  else {
+  } else {
     t = 2 - m;
   }
   return t;
 }
 
-bool ObjectInUGB(Mat& img, Point2i q, int ugb, int gsize){
+bool ObjectInUGB(Mat& img, Point2i q, int ugb, int gsize) {
   Point2i pt;
-  switch (ugb){
-    case 1: 
+  switch (ugb) {
+    case 1:
       pt.x = q.x;
       pt.y = q.y - gsize;
       // cout << "debug info: 1 " << pt << endl;
       break;
     case 2:
-      pt.x = q.x -gsize;
+      pt.x = q.x - gsize;
       pt.y = q.y - gsize;
       // cout << "debug info: 2 " << pt << endl;
       break;
@@ -187,16 +191,69 @@ bool ObjectInUGB(Mat& img, Point2i q, int ugb, int gsize){
     default:
       break;
   }
-  uchar *p;
-  for(int i=pt.y; i < pt.y + gsize; i++){
+  uchar* p;
+  for (int i = pt.y; i < pt.y + gsize; i++) {
     p = img.ptr(i);
-    for (int j = pt.x; j < pt.x + gsize; ++j)
-    {
+    for (int j = pt.x; j < pt.x + gsize; ++j) {
       // cout << "(" << i <<"," << j << ")" << int(p[j]);
-      if(p[j] == 0){
+      if (p[j] == 0) {
         return false;
       }
     }
   }
   return true;
+}
+
+Point2i getNextPoint(Point2i currentpoint, int d, int gsize) {
+  Point2i nextpoint;
+  switch (d) {
+    case 0:
+      nextpoint.x = currentpoint.x + gsize;
+      nextpoint.y = currentpoint.y;
+      break;
+    case 1:
+      nextpoint.x = currentpoint.x;
+      nextpoint.y = currentpoint.y + gsize;
+      break;
+    case 2:
+      nextpoint.x = currentpoint.x - gsize;
+      nextpoint.y = currentpoint.y;
+      break;
+    case 3:
+      nextpoint.x = currentpoint.x;
+      nextpoint.y = currentpoint.y - gsize;
+      break;
+  }
+  return nextpoint;
+}
+
+vector<Point2i> makeOIP(Mat& img, Point2i topleftpoint, int gsize) {
+  vector<Point2i> vertices;
+  Point2i startpoint = getStartPoint(img, topleftpoint, gsize);
+  Point2i q = startpoint;
+  int type = getPointType(img, q, gsize);
+  int d = (2 + type) % 4;
+
+  // show the steps
+  steps = final.clone();
+  circle(steps, q, 1, CV_RGB(0, 255, 100), 1, CV_AA, 0);
+  imshow("Final image", steps);
+  waitKey(1000);
+
+  do {
+    if (type == 1 || type == -1) {
+      vertices.push_back(q);
+      circle(steps, q, 3, CV_RGB(255, 0, 0), 1, CV_AA, 0);
+    }
+    q = getNextPoint(q, d, gsize);
+    type = getPointType(img, q, gsize);
+    circle(steps, q, 1, CV_RGB(0, 0, 200), 1, CV_AA, 0);
+    if (type == -2) {
+      type = -1;
+    }
+    d = (d + type) % 4;
+    imshow("Final image", steps);
+    waitKey(1000);
+  } while (q != startpoint);
+  return vertices;
 }
